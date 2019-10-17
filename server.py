307069470791e6433cd9959c0dbae6c5a612b4cc
Glitch (flask-demo) <none>
2019-10-17 18:33:20 +0000

    🎓🌥 Checkpoint ./server.py:622305/966 ./templates/index.html:622305/1678

--- server.py
+++ server.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template("index.html")
+
+@app.route('/character', methods=['GET', 'POST'])
+def computeCharacter():
+  if request.method == 'POST':
+    userdata = dict(request.form)
+    # print(userdata)
+    character = ""
+    return "You said " + str(userdata["book"]) + "!"
+  else:
+    return "Sorry, there was an error."
+  
+
 
 if __name__ == "__main__":
   app.run()
