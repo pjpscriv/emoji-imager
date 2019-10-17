61988f054917497c22438101d9e1245700972a6b
Glitch (flask-demo) <none>
2019-10-17 19:00:41 +0000

    🏅🍇 Checkpoint ./models/model.py:622305/165 ./server.py:622305/337 ./model.py:622305/716 ./templates/index.html:622305/48 ./templates/result.html:622305/1166

--- models/model.py
+++
@@ -1,9 +0,0 @@
-def compute_character(book):
-  if book == "bible":
-    return "Angela"
-  elif book == "davinci":
-    return "Phyllis"
-  elif book == "harrypotter"
-    return "Dwight"
-  else:
-    return None
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 import model
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,8 +15,9 @@ def result():
   if request.method == 'POST':
     userdata = dict(request.form)
     book = userdata["book"][0]
-    character = model.compute_character(book)
-    return "You're " + character + "!"
+    character = model.get_character(book)
+    gif_url = model.get_gif(character)
+    return render_template("result.html", character=character, gif_url=gif_url)
   else:
     return "Sorry, there was an error."
   
