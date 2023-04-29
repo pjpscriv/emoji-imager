d3622a6e8c9821678aa8d47dda0e9a2ba95a8ed9
Glitch (flask-demo) <none>
2023-04-29 23:43:13 +0000

    📠📌 Checkpoint ./static/js/index.js:28657391/472 ./templates/index.html:28657391/163

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,30 @@
  $('#start-color').on('input', () => {
-   printValues()
+   printValues();
+   updateImage();
  });
 
  $('#end-color').on('input', () => {
-   printValues()
+   printValues();
+   updateImage();
  });
 
  $('#emoji').on('input', () => {
-   printValues()
+   printValues();
+   updateImage();
  });
 
 function printValues() {
   console.log('Start:', $('#start-color').val());
   console.log('End  :', $('#end-color').val());
   console.log('Emoji:', $('#emoji').val());
+}
+
+function updateImage() {
+  let start = $('#start-color').val().replace('#', '');
+  let end   = $('#end-color').val().replace('#', '');
+  let emoji = $('#emoji').val();
+  
+  let new_src = `/image?start=${start}&end=${end}&emoji=${emoji}`;
+  
+  $('.image-result img').attr('src', new_src);
 }
