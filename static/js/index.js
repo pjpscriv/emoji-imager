1ba072a415fbac2d586a79b84747dbf93e944a6e
Peter Scriven <pjpscriv@gmail.com>
2025-06-21 17:23:12 -0400

    Add Emoji 'flavour' picker to UI

--- static/js/index.js
+++ static/js/index.js
@@ -13,6 +13,11 @@
    updateImage();
  });
 
+  $('#flavour').on('change input', () => {
+    printValues();
+    updateImage();
+  });
+
 function printValues() {
   console.log('Start:', $('#start-color').val());
   console.log('End  :', $('#end-color').val());
@@ -23,9 +28,10 @@ function updateImage() {
   let start = $('#start-color').val().replace('#', '');
   let end   = $('#end-color').val().replace('#', '');
   let emoji = $('#emoji').val();
+  let vendor = $('#flavour').val();
   
-  let new_src = `/image?start=${start}&end=${end}&emoji=${emoji}`;
-  let new_href = `/${start}/${end}/${emoji}`;
+  let new_src = `/image?start=${start}&end=${end}&emoji=${emoji}&vendor=${vendor}`;
+  let new_href = `/${start}/${end}/${emoji}/${vendor}`;
   
   $('.image-result img').attr('src', new_src);
   $('.download-link a').attr('href', new_src);
